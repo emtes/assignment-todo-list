87bb053acf43db126f8d9940e18a79fb58d153bd
Enmanuel dlN <edelanuezlang@gmail.com>
2020-03-20 01:51:51 -0400

    hack: toggle edit mode for task

--- public/javascripts/taskActions.js
+++ public/javascripts/taskActions.js
@@ -1,12 +1,64 @@
 const toggleEdit = (taskId) => {
-  /*
-  Grab task wrapper
-  Add forms
-  When form submits, call editTask
-  */
+  const taskWrapper = document.getElementById(taskId);
+  const task = taskWrapper.querySelector('.task');
+  const description = taskWrapper.querySelector('.description');
+  const date = taskWrapper.querySelector('.dueDate');
+
+  task.innerHTML = newInput('Task', 'task', 'text', [task.innerText]);
+  description.innerHTML = newTextArea('Description', 'description', [description.innerText]);
+  date.innerHTML = newInput('Due Date', 'dueDate', 'date', []);
+  disableForms('task-update-input');
+
+  const submitButtonAttributes = {
+    onclick: `editTask(
+      ${taskId},
+      ${task.value},
+      ${description.value},
+      ${date.value}
+    )`,
+  };
+  const submitButton = createElement('button', submitButtonAttributes);
+  taskWrapper.appendChild(submitButton);
 };
 
-const editTask = () => {};
+const editTask = (taskId, task, description, date) => {
+  console.log(`Ready to edit task ${taskId}, with:`, task, description, date);
+};
+
+function createElement(tag, attributes) {
+  const el = document.createElement(tag);
+  for (const att in attributes) {
+    el.setAttribute(att, attributes[att]);
+  }
+  return el;
+}
+
+function newInput(label, name, inputType, currentData) {
+  return `
+  <form class="task-update-input">
+    <label for="newTask">${label}</label>
+    <input type="${inputType}" name="${name}"
+    id="newTask" value="${currentData[0] ? currentData[0] : ''}" required>
+  </form>
+  `;
+}
+
+function newTextArea(label, name, currentData) {
+  return `
+  <form class="task-update-input">
+    <label for="newTask">${label}</label>
+    <textarea name="${name}" id="description"
+    rows="8" cols="32">${currentData[0]}</textarea>
+  </form>
+  `;
+}
+
+function disableForms(querySelector) {
+  const forms = document.querySelectorAll(querySelector);
+  for (const form of forms) {
+    form.addEventListener('submit', (e) => { e.preventDefault(); });
+  }
+}
 
 const toggleComplete = (taskId) => {
   const toggleCompleteInit = {
